2adb789d6da96d12a99a73c0a24341a350915713
Veljko Stanojevic <veljko.stanojevic@florencehc.com>
2021-07-02 14:14:15 +0200

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './components/layout/layout.component';
+import { HomeComponent } from './components/home/home.component';
+import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LayoutComponent,
+    HomeComponent,
+    UsersComponent,
   ],
   imports: [
     BrowserModule,
